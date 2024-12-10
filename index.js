76d4941d33f145622e53ec43fc1149ee8a28f4be
Bauduin Raphaël <raphael@asfaload.com>
2024-12-10 11:54:40 +0100

    import os

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
+const os = require("os");
 
 function getSuffix() {
   switch (process.platform, process.arch) {
